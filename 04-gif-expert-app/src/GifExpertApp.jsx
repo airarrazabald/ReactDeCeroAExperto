db0b39e1c61d2a5426b2c3bda9befc570aebe7ac
Abelardo Ruben Irarrazabal Diaz <airarrazabal@consalud.cl>
2022-06-30 23:55:46 -0400

    Agregar una nueva categoria

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,4 +1,14 @@
+
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => setCategories( cat => [ ...cat,'KOF']); 
+
+    console.log(categories);
+
     return (
         <>
             {/*titulo */}
@@ -7,6 +17,10 @@ export const GifExpertApp = () => {
             {/* input */}
 
             {/* Listado de gifs*/}
+            <button onClick={ onAddCategory } >Agregar</button>
+            <ol>
+                { categories.map( category =>  <li key={ category }> { category } </li> )}
+            </ol>
                 {/* GIF Item */}
         </>
     );
