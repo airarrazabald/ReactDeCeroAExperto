2a4bc89287024190e20c43d2d216b11fe9f1901e
Abelardo Ruben Irarrazabal Diaz <airarrazabal@consalud.cl>
2022-07-01 23:47:28 -0400

    Se agrega archivo de barril

--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async( category ) => {
     const apiKey = '7FNIkB7rpJ40EUkRHYf8vzenFgY91Fd4';
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=6`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=12`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
