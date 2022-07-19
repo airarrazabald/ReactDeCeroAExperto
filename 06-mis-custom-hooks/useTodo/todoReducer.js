8ea1737df1c8ad49f5b362cbbc7f1c8ff7c9232e
Abelardo Irarrázabal <abelardo.irarrazabal@consalud.cl>
2022-07-18 22:37:13 -0400

    Usetodos agregados

---
+++ 06-mis-custom-hooks/useTodo/todoReducer.js
@@ -0,0 +1,25 @@
+
+export const todoReducer = ( initialState =[], action) => {
+
+
+    switch (action.type) {
+        case 'AddTodo':
+            return [...initialState,action.payload];
+        case 'RemoveTodo':
+            return initialState.filter(todo => todo.id !== action.payload);
+        case 'ToggleTodo':
+            return initialState.map(todo => {
+                if(todo.id === action.payload) {
+                    return { 
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+                else{
+                    return { ...todo}
+                }
+            });
+        default:
+            return initialState;
+    }
+}
