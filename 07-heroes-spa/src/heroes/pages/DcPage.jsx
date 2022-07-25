dedb6f9d43b6cad58eda585fd5d776a6cddcb970
Abelardo Irarrázabal <abelardo.irarrazabal@consalud.cl>
2022-07-25 17:44:43 -0400

    Card con infromación de heroes

--- 07-heroes-spa/src/heroes/pages/DcPage.jsx
+++ 07-heroes-spa/src/heroes/pages/DcPage.jsx
@@ -1,6 +1,19 @@
+import { HeroList } from '../components';
+
 
 export const DcPage = () => {
+
   return (
-    <h1>DcPage</h1>
+    <>
+      <h1>DC Comics</h1>
+      <hr />
+
+      <ul>
+       {
+          <HeroList publisher="DC Comics" />
+       }
+      </ul>
+
+    </>
   )
 }
